82875025dfcfb519174a17d77d84aa9dfd65fe08
shahmeershah98 <52623706+shahmeershah98@users.noreply.github.com>
2020-03-30 19:40:43 +0500

    Timer with TimeNote

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-
+//Seconds for Timer
   const [seconds,setSeconds] = useState(0);
+// active is use for pausing timer 
   const [active,setActive] = useState(false);
+// this state is an array in which we push noted times
+  const [times,setTimes] = useState({note: []})
 
 
   
@@ -18,6 +21,17 @@ function App() {
     return ()=>clearInterval(fun)
   },[seconds,active])
 
+
+  const [value,setValue] = useState(0);
+
+  useEffect(()=>{
+    if(value>0){
+      times.note.push(seconds)
+      console.log(times.note);
+    }
+  },[value]);
+
+
   return(
 
     <div className="App">
@@ -26,6 +40,19 @@ function App() {
       <br/>
       <button onClick={()=>setActive(active?false:true)}>{active?"Pause":"Start"}</button>
       <button onClick={()=>setSeconds(0)}>Reset</button>
+      <button onClick={()=>setValue(value+1)}>Note Time</button>
+      <br />
+      <ul className="ulfortimer">
+        {times.note.map((val)=>{
+          return(
+            <li>
+              {`${val}`}
+            </li>
+          )
+        })}
+      </ul>
+      <br/>
+      <button onClick={()=>{times.note.length=0}}>Reset Noted Times</button>
       
 
 
